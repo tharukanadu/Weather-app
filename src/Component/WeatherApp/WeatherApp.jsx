6c63d066a7ec18f6acb3f7db63527d5ce608c40e
tharukanadu <tharukanadun279@gmail.com>
2023-12-12 10:20:12 +0530

    add icon

--- src/Component/WeatherApp/WeatherApp.jsx
+++ src/Component/WeatherApp/WeatherApp.jsx
@@ -5,7 +5,7 @@ import cloud from '../Assets/cloud.png'
 import drizzle from '../Assets/drizzle.png'
 import humidity from '../Assets/humidity.png'
 import rain from '../Assets/rain.png'
-import search from '../Assets/search.png'
+import searchh from '../Assets/search.png'
 import snow from '../Assets/snow.png'
 import wind from '../Assets/wind.png'
 
@@ -62,7 +62,7 @@ const search = async ()=>{
      <div className="top-bar">
       <input type="text" className='cityInput' placeholder='search' />
       <div className="search_icon" onClick={()=>{search()}}>
-        <img src={search} alt="" />
+        <img src={searchh} alt="" />
       </div>
      </div>
      <div className="weather-image">
